ef53222bc63536caa1b72fc46b65ef3cb36011d6
Ricardo <ricardo.velez96@gmail.com>
2024-01-25 19:25:47 +0000

    Add views

---
+++ app/src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>This is an about page to demo the router!</h1>
+  </div>
+</template>
+
+
---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Header from '@/components/Header/Header.vue'
+import TranscriptionList from '@/components/TranscriptionList/TranscriptionList.vue'
+</script>
+
+<template>
+  <Header />
+  <TranscriptionList />
+</template>
+
